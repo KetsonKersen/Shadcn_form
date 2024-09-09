646f55e36aecf5a12fde82bd156c16882a02cf8c
Ketson Kersen (Estagio) <k.kersen@hotmail.com>
2024-09-09 13:18:06 -0300

    fix: calc logic

--- src/app/form_products/functions/calcMoreInf.ts
+++ src/app/form_products/functions/calcMoreInf.ts
@@ -1,6 +1,7 @@
 import { useEffect, useMemo } from "react"
 import { useFormContext, useWatch } from "react-hook-form"
 import { useProductsStore } from "../store/store"
+import { Iproduct } from "../types/types"
 
 export const setValuesMoreInf = ()=>{
     const useStore = useProductsStore()
@@ -11,35 +12,37 @@ export const setValuesMoreInf = ()=>{
     let totalVolume = 0
 
     useEffect(()=>{
-        useStore.products.map((product)=>{
+        useStore.products.map((product:Iproduct)=>{
             totalProdutoServico += Number(product.valor_total?.replace("R$","").replace(",",""))
             totalPeso += Number(product.quantidade?.replace("X","").replace(",","")) * Number(product.peso?.replace("kg","").replace(",",""))
             totalVolume +=  Number(product.quantidade?.replace("X","").replace(",","")) * Number(product.volume?.replace("uni","").replace(",",""))
         })
-        setValue("total_produto_servico", totalProdutoServico)
-        setValue("peso_total",totalPeso)
-        setValue("volume_total",totalVolume)
-    },[useStore])
+
+        setValue("total_nota", totalProdutoServico != 0 ? "R$"+totalProdutoServico : "")
+        setValue("total_produto_servico", totalProdutoServico != 0 ? "R$"+totalProdutoServico : "")
+        setValue("peso_total", totalPeso != 0 ? totalPeso+"kg" : "")
+        setValue("volume_total", totalVolume != 0 ? totalVolume+"uni" : "")
+    },[useStore]) 
 }
 
 export const calcTotalNota = ()=>{
-    const { setValue } = useFormContext()
+    const { setValue, watch} = useFormContext()
     
-    let frete = useWatch({name:"frete"})
-    let desconto = useWatch({name:"desconto"})
-    let total_produto_servivo = useWatch({name:"total_produto_servico"})
+    let frete = watch("frete")
+    let desconto = watch("desconto")
+    let totalProdutoServico = watch("total_produto_servico")
 
     if(frete == undefined) frete = "0"
     if(desconto == undefined) desconto = "0"
-    if(total_produto_servivo == undefined) total_produto_servivo = "0"
+    if(totalProdutoServico == undefined) totalProdutoServico = "0"
     
     useMemo(()=>{
-        const v_frete = Number(frete?.replace("R$","").replace(",",""))
-        const v_desconto = Number(desconto?.replace("R$","").replace(",",""))
-        const v_total = Number(JSON.stringify(total_produto_servivo)?.replace("R$","").replace(",",""))
-        const res = v_total + v_frete - v_desconto
+        const v_frete = frete?.replace("R$","")?.replace(",","")
+        const v_desconto = desconto?.replace("R$","").replace(",","")
+        const v_total = totalProdutoServico?.replace("R$","").replace(",","")
+        const res = Number(v_total) + Number(v_frete) - Number(v_desconto)
         
-        setValue("total_nota", "R$" + res)
-    },[frete,desconto,total_produto_servivo])
+        setValue("total_nota", +res)
+    },[frete,desconto,totalProdutoServico])
 }
 
--- src/app/form_products/functions/calcSetProduct.ts
+++ src/app/form_products/functions/calcSetProduct.ts
@@ -1,18 +1,18 @@
-import { useMemo } from "react"
+import { useEffect, useMemo } from "react"
 import { useFormContext, useWatch} from "react-hook-form"
 
 export const calcValorTotal = ()=>{
-    const { setValue } = useFormContext()
+    const { setValue, watch} = useFormContext()
     
-    let quantidade = useWatch({name:"quantidade"})
-    let valor = useWatch({name:"valor_uni"})
-
-    useMemo(()=>{
+    let quantidade = watch("quantidade")
+    let valor = watch("valor_uni")
+    
+    useEffect(()=>{
         quantidade = quantidade?.replace("X","").replace(".","").replace(",","").trim()
         valor = valor?.replace("R$","").replace(".","").replace(",","").trim()
 
-        const res = Number(quantidade) * Number(valor)
-
-        setValue("valor_total", "R$" + res)
+        const res = JSON.stringify(Number(quantidade) * Number(valor))
+        setValue("valor_total",  res != "0" ? "R$"+res : "")
+    
     },[quantidade,valor])
 }
